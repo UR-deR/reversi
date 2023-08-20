3c7e32902e68bc8da55b204763dca3822f92cff0
william <shota.furuno@claves.co.jp>
2023-08-20 18:31:53 +0900

    format code by prettier

--- static/main.js
+++ static/main.js
@@ -1,6 +1,6 @@
-const EMPTY = 0
-const DARK = 1
-const LIGHT = 2
+const EMPTY = 0;
+const DARK = 1;
+const LIGHT = 2;
 
 const INITIAL_BOARD = [
   [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
@@ -10,30 +10,35 @@ const INITIAL_BOARD = [
   [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
   [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
   [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]
-]
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+];
 
-const boardElement = document.getElementById('board')
+const boardElement = document.getElementById('board');
 
 async function showBoard() {
   while (boardElement.firstChild) {
-    boardElement.removeChild(boardElement.firstChild)
+    boardElement.removeChild(boardElement.firstChild);
   }
 
   INITIAL_BOARD.forEach((line) => {
     line.forEach((square) => {
       // <div class="square">
-      const squareElement = document.createElement('div')
-      squareElement.className = 'square'
+      const squareElement = document.createElement('div');
+      squareElement.className = 'square';
 
       if (square !== EMPTY) {
         // <div class="stone dark">
-        const stoneElement = document.createElement('div')
-        const color = square === DARK ? 'dark' : 'light'
-        stoneElement.className = `stone ${color}`
+        const stoneElement = document.createElement('div');
+        const color = square === DARK ? 'dark' : 'light';
+        stoneElement.className = `stone ${color}`;
 
-        squareElement.appendChild(stoneElement)
+        squareElement.appendChild(stoneElement);
       }
+
+      boardElement.appendChild(squareElement);
+    });
+  });
+}
 
 async function registerGame() {
   await fetch('/api/games', {
@@ -46,4 +51,4 @@ async function main() {
   await showBoard();
 }
 
-main()
+main();
