5067653208ef494acb65d90700d6facaa43294d6
william <shota.furuno@claves.co.jp>
2023-08-20 18:54:11 +0900

    record initial board state on registering game

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,21 @@ import morgan from 'morgan';
 import 'express-async-errors';
 import mysql from 'mysql2/promise';
 
+const EMPTY = 0;
+const DARK = 1;
+const LIGHT = 2;
+
+const INITIAL_BOARD = [
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, DARK, LIGHT, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+] as const;
+
 const app = express();
 
 const PORT = 3000;
@@ -23,7 +38,7 @@ app.get('/error', (req, res) => {
 });
 
 app.post('/api/games', async (req, res) => {
-  const startedAt = new Date();
+  const now = new Date();
   const dbConnection = await mysql.createConnection({
     host: 'localhost',
     database: 'reversi',
@@ -33,7 +48,32 @@ app.post('/api/games', async (req, res) => {
 
   try {
     await dbConnection.beginTransaction();
-    await dbConnection.execute('INSERT INTO games (started_at) VALUES (?)', [startedAt]);
+
+    const [gameInsertResult] = await dbConnection.execute<mysql.ResultSetHeader>(
+      'INSERT INTO games (started_at) VALUES (?)',
+      [now]
+    );
+
+    const gameId = gameInsertResult.insertId;
+
+    const [turnInsertResult] = await dbConnection.execute<mysql.ResultSetHeader>(
+      'INSERT INTO turns (game_id, turn_count, next_disc, end_at) VALUES (?, ?, ?, ?)',
+      [gameId, 0, DARK, now]
+    );
+
+    const turnId = turnInsertResult.insertId;
+
+    const squareCount = INITIAL_BOARD.map((row) => row.length).reduce((a, b) => a + b, 0);
+
+    const squaresInsertSql =
+      'INSERT INTO squares (turn_id, x, y, disc) VALUES ' +
+      Array.from(Array(squareCount))
+        .map(() => '(?, ?, ?, ?)')
+        .join(', ');
+
+    const squaresInsertValues = INITIAL_BOARD.flatMap((row, y) => row.map((state, x) => [turnId, x, y, state])).flat();
+
+    await dbConnection.execute(squaresInsertSql, squaresInsertValues);
     await dbConnection.commit();
   } finally {
     await dbConnection.end();
